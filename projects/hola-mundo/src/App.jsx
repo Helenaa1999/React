54e83dcdb91175a8d11a5f6808b3ec4991dc4f72
Helena Peño Carrera <110026677+Helenaa1999@users.noreply.github.com>
2025-01-02 18:17:28 +0100

    Hola mundo

---
+++ projects/hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFolowCard'
+export function App (){
+    return (
+        <section>
+       <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán"/>
+       <TwitterFollowCard userName="pheralb" name="Miguel Ángel Durán"/>
+       <TwitterFollowCard userName="vxnder" name="Miguel Ángel Durán"/>
+       </section>
+    )
+}
